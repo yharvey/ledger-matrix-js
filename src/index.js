71366246392a78771a0dbdea92220dde0a94a63d
Juan Leni <lenijuan@gmail.com>
2019-05-27 18:17:15 +0200

    getAddress

--- src/index.js
+++ src/index.js
@@ -15,46 +15,86 @@
  *  limitations under the License.
  ******************************************************************************* */
 
-import Transport from "@ledgerhq/hw-transport";
-import { TransportStatusError } from "@ledgerhq/hw-transport";
+import Transport from '@ledgerhq/hw-transport';
+import { TransportStatusError } from '@ledgerhq/hw-transport';
 
 const CLA = 0x88;
 
 const INS = {
-    GET_VERSION: 0x00
+    GET_VERSION: 0x00,
+    GETADDR_SECP256K1: 0x01,
+    SIGN_SECP256K1: 0x02,
 };
 
 
 export default class MatrixAIApp {
-    constructor(transport, scrambleKey = "man") {
-        if (typeof transport == 'undefined'){
-            throw "Transport has not been defined";
+    constructor(transport, scrambleKey = 'man') {
+        if (typeof transport == 'undefined') {
+            throw new Error('Transport has not been defined');
         }
 
         this.transport = transport;
         transport.decorateAppAPIMethods(
             this,
-            [],
-            scrambleKey
+            [
+                'getVersion',
+            ],
+            scrambleKey,
         );
     }
 
     async getVersion() {
-        return this.transport.send(CLA, INS.GET_VERSION, 0, 0).then(
-            response => {
-                console.log(response);
-                let error_code_data = response.slice(-2);
-                return {
-                    test_mode: response[0] !== 0,
-                    major: response[1],
-                    minor: response[2],
-                    patch: response[3],
-                    device_locked : response[4] === 1,
-                    return_code: error_code_data[0] *256 + error_code_data[1],
-                    // TODO: Improve error handle
-                    // TODO: Unify error messages
-                    error_message: "????"
-                }
-            });
+        return this.transport.send(CLA, INS.GET_VERSION, 0, 0)
+            .then(
+                (response) => {
+                    const errorCodeData = response.slice(-2);
+                    return {
+                        test_mode: response[0] !== 0,
+                        major: response[1],
+                        minor: response[2],
+                        patch: response[3],
+                        device_locked: response[4] === 1,
+                        return_code: errorCodeData[0] * 256 + errorCodeData[1],
+                        // TODO: Improve error handle
+                        // TODO: Unify error messages
+                        error_message: '????'
+                    };
+                },
+            );
     }
+
+    static serializeMANBIP44(account, change, addressIndex) {
+        const buf = Buffer.alloc(20);
+        buf.writeUInt32LE(0x8000002c, 0);
+        buf.writeUInt32LE(0x8000013e, 4);
+        // eslint-disable-next-line no-bitwise
+        buf.writeUInt32LE(0x80000000 + account, 8);
+        // eslint-disable-next-line no-bitwise
+        buf.writeUInt32LE(change, 12);
+        // eslint-disable-next-line no-bitwise
+        buf.writeUInt32LE(addressIndex, 16);
+
+        return buf;
+    }
+
+    async getAddress(account, change, addressIndex) {
+        const bip44Path = MatrixAIApp.serializeMANBIP44(account, change, addressIndex);
+        return this.transport.send(CLA, INS.GETADDR_SECP256K1, 0, 0, bip44Path)
+            .then(
+                (response) => {
+                    const errorCodeData = response.slice(-2);
+                    return {
+                        pubKey: response.slice(0, 65)
+                            .toString('hex'),
+                        address: response.slice(65, 97)
+                            .toString('ascii'),
+                        return_code: errorCodeData[0] * 256 + errorCodeData[1],
+                        // TODO: Improve error handle
+                        // TODO: Unify error messages
+                        error_message: '????',
+                    };
+                },
+            );
+    }
+
 }
--- tests/basic.ispec.js
+++ tests/basic.ispec.js
@@ -1,10 +1,28 @@
 import MatrixAIApp from 'index.js';
-import TransportNodeHid from "@ledgerhq/hw-transport-node-hid";
+import TransportNodeHid from '@ledgerhq/hw-transport-node-hid';
 
-test('example integration test', async () => {
+test('get version', async () => {
     const transport = await TransportNodeHid.create(1000);
-    const app = new MatrixAIApp(transport);
+    transport.setDebugMode(true);
 
-    let version = await app.getVersion();
+    const app = new MatrixAIApp(transport);
+    const version = await app.getVersion();
     console.log(version);
+});
+
+test('get address', async () => {
+    const transport = await TransportNodeHid.create(1000);
+    transport.setDebugMode(true);
+
+    const app = new MatrixAIApp(transport);
+    const response = await app.getAddress(0, 0, 0);
+
+    expect(response.pubKey)
+        .toEqual(
+            '0491c5822f1e8e096d5834c19f53933d9e1d9c653a52c7b7f27e35a202bb4d7d7'
+            + '585f3fdd3d697185b9cd78a5d571281d7d96225042aa4bf26fec7b32d130416e7',
+        );
+
+    expect(response.address)
+        .toEqual('MAN.cUTaQZsmCAdpshzWnFiatff8QZHv');
 });
